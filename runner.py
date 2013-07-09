dde1454cf362f4f4755d6c9b2a72c3368414e0f3
Simon Chopin <chopin.simon@gmail.com>
2013-07-09 16:13:15 +0200

    Add bugs support to the runner script

--- runner.py
+++ runner.py
@@ -5,11 +5,16 @@ import sys
 import hy.importer
 from pyinotify import WatchManager, IN_MOVED_TO, Notifier, ProcessEvent
 
-from debmessenger import changes
+from debmessenger import changes, bugs
+
+hooks = {
+        sys.argv[1]: changes.hook,
+        sys.argv[2]: bugs.hook,
+        }
 
 class EventHandler(ProcessEvent):
     def process_IN_MOVED_TO(self, event):
-        changes.hook(event.pathname)
+        hooks[event.path](event.pathname)
 
 wm = WatchManager()
 wm.add_watch(sys.argv[1], IN_MOVED_TO)
